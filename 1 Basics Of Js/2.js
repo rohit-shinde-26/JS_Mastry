fae5bed4e0cac8c34d8c6c749a665a652c0f8b42
Rohit <rohitshinde26072003@gmail.com>
2024-11-10 10:42:03 +0530

    declare variable using var

---
+++ 1 Basics Of Js/2.js
@@ -0,0 +1,10 @@
+// Declare variable using var 
+
+var a = 32;
+var b = "Abhay";
+var c = true;
+var d = 89.765;
+
+
+// Q) Why use of var ?
+// => Because, when we can call a variable before declaration it will be call. 
