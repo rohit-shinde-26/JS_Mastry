8cec9c9336b942556750cc0e4f9ecf1c8b4a6256
Rohit <rohitshinde26072003@gmail.com>
2024-11-10 10:48:32 +0530

    let

---
+++ 1 Basics Of Js/4.js
@@ -0,0 +1,13 @@
+// Let keyword 
+let name = "Ram";
+
+// It is used when declaring a variable 
+// It is update but not redeclare
+
+let firstName = "Sham";
+// let firstName = "Abhi"   (Invalid)
+
+firstName = "Abhi";
+
+
+console.log(firstName)
