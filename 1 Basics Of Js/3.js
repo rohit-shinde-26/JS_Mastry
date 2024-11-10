4f3c581551f5da004ea75cab3cf721d380ba14bd
Rohit <rohitshinde26072003@gmail.com>
2024-11-10 10:44:26 +0530

    rules for naming variables

---
+++ 1 Basics Of Js/3.js
@@ -0,0 +1,27 @@
+
+// rules for naming variables 
+
+// you cannot start with number 
+// example :- 
+// 1value (invalid)
+// value1 (valid)
+
+var value1 = 2;
+console.log(value1);
+console.log(value1 ** 3);  // power of value
+console.log(value1 ** 0.5); // square root
+
+// you can use only undersore _ or dollar symbol 
+// first_name (valid)
+// _firstname (valid) 
+
+// first$name (valid)
+// $firstname (valid)
+
+// you cannot use spaces 
+// var first_name = "Rohit"; // snake case writing 
+// var firstName = "rohit"; // camel case writing 
+// first name (invalid)
+
+// convention for pro
+// start with small letter and use camelCase 
