06152acfa8b75eeb37229143585d0ae43ffb9421
Rohit <rohitshinde26072003@gmail.com>
2024-11-13 19:47:12 +0530

    break and continue keyword

---
+++ 1 Basics Of Js/23.js
@@ -0,0 +1,16 @@
+// Break and Continue keyword
+
+let i = 0;
+
+while(i<=10){
+    i++;
+    
+    // if(i==5){
+    //     break;
+    // }
+    
+    if(i==5){
+        continue;
+    }
+    console.log(i);
+}
