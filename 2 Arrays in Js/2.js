81af5ecdf87718822e960d27057885d7f9f63c6a
Rohit <rohitshinde26072003@gmail.com>
2024-11-14 20:40:59 +0530

    push pop shift unshift

---
+++ 2 Arrays in Js/2.js
@@ -0,0 +1,24 @@
+// push pop shift unshift 
+
+let myCars = ["mustang","Lambo","Porshe","GTR"]
+console.log(myCars)
+
+// Push
+// It is adds the element at last of an array
+// myCars.push("Supra")
+// console.log(myCars)
+
+//pop
+// It removes last element from an array
+// myCars.pop()
+// console.log(myCars)
+
+// unshift
+// It adds element at start position of an array
+// myCars.unshift("Dodge")
+// console.log(myCars)
+
+// shift
+// It removes start element from an array
+myCars.shift()
+console.log(myCars)
