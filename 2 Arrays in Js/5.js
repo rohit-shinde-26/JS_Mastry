c813775b614438db69a432fdd6fe150aa2bbcc3d
Rohit <rohitshinde26072003@gmail.com>
2024-11-16 22:18:45 +0530

    for loop in array

---
+++ 2 Arrays in Js/5.js
@@ -0,0 +1,11 @@
+// for loop 
+
+let cars = ["Mustang","Supra","Lambo","M5 Sport"]
+let final = []
+for(let i=0;i<cars.length;i++){
+    final.push(cars[i].toUpperCase())
+}
+console.log(final)
+
+
+
