903c57d8cc170df71024eff23a6b8d9f7bed8f24
Rohit <rohitshinde26072003@gmail.com>
2024-11-19 18:30:04 +0530

    while loop in array

---
+++ 2 Arrays in Js/7.js
@@ -0,0 +1,9 @@
+// while loop in array 
+let i = 0;
+const cars = ["GTR","Dodge","Mustang"]
+const ans = [];
+while(i<cars.length){
+    ans.push(cars[i].toUpperCase())
+    i++
+}
+console.log(ans)
